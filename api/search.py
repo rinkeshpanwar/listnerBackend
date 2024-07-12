2462e8f187b85bd3d5c165fdd7fbafc55a0bb4bf
rinkesh panwar <62168730+rinkeshpanwar@users.noreply.github.com>
2024-07-12 16:48:57 +0000

    setup docker

--- api/search.py
+++ api/search.py
@@ -1,11 +1,11 @@
 from fastapi import APIRouter, Depends
 from api.auth import get_current_user
 from schema.search import SearchBase
-from crud.search import CRUDSearch
+# from crud.search import CRUDSearch
 
 router = APIRouter()
 
-@router.post("/")
-def get_search_question(search: SearchBase, user = Depends(get_current_user)):
-    return CRUDSearch.search_question(search)
+# @router.post("/")
+# def get_search_question(search: SearchBase, user/ = Depends(get_current_user)):
+#     return CRUDSearch.search_question(search)
 
